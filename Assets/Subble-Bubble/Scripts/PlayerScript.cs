2ddd5e31c7e383fae7709ac6585133033c54fca0
Fran <matheo.davila@saei.mx>
2025-01-26 00:41:13 -0600

    Playerovement
    PlayerMovement
    Flash
    Freno

--- Assets/Subble-Bubble/Scripts/PlayerScript.cs
+++ Assets/Subble-Bubble/Scripts/PlayerScript.cs
@@ -1,161 +1,263 @@
-using System.Collections;
-using Unity.VisualScripting;
+using UnityEditor.PackageManager;
 using UnityEngine;
-using UnityEngine.Animations;
-using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class BurbujaScript : MonoBehaviour
 {
+    public Image _cooldown;
+
+    [Header("Movement")]
     [SerializeField] private Rigidbody2D _rb;
     [SerializeField] private float _gravityScale;
+
     [SerializeField] private Vector2 _sideMove;
     [SerializeField] private float _hopDuration;
+    [SerializeField] private float _timeBetweenHops;
+    public float _midTimeCounter = 0.0f;
+    public float _hopTimeCounter = 0.0f;
+    public bool _canHop = true;
+
     [SerializeField] private float _decelerate;
+    public bool mousePressed = false;
 
-    public float _raycastDistancia = 10f; // Distancia del raycast
-    public float _velocidadRotacion = 5f; // Velocidad de rotación del raycast (basado en el mouse)
-    public LayerMask _capaInteractuable; // Capa que el raycast puede impactar (por ejemplo, objetos que interactúan con la burbuja)
+    [Header("Raycast")]
+    [SerializeField] private float _raycastDistancia = 10f;
+    [SerializeField] private float _velocidadRotacion = 5f;
+    [SerializeField] private LayerMask _capaInteractuable;
 
-    public Transform _origenRaycast; // El Transform del origen del raycast (puedes asignar cualquier GameObject aquí)
-    private RaycastHit _hitInfo; // Información del raycast
-    private Vector2 _raycastDireccion; // Dirección del raycast
+    [SerializeField] private Transform _origenRaycast;
+    private RaycastHit _hitInfo;
+    private Vector2 _raycastDireccion;
 
-    [SerializeField] private int _maxSpeed; // The max speed the capsule can axelerate to.
-    [SerializeField] private int _minSpeed; // The min speed the capsule can axelerate to.
+    [SerializeField] private int _maxSpeed;
+    [SerializeField] private int _minSpeed;
 
-    private Keyboard _keyboard;
-    private float _counter = 0.0f;
-    private bool _detectInput = true;
+    [Header("Oxygen")]
+    [SerializeField] public int Oxygen;
+    [SerializeField] private int OxygenMax;
+
+    [Header("Horizontal Velocity")]
+    // Velocidades modificables para cada tag
+    [SerializeField] private float velocidadIzquierda1;
+    [SerializeField] private float velocidadIzquierda2;
+    [SerializeField] private float velocidadDerecha1;
+    [SerializeField] private float velocidadDerecha2;
+
+    [Header("Flash")]
+    [SerializeField] public GameObject FlashIzq;
+    [SerializeField] public GameObject FlashDer;
+    [SerializeField] GameObject _changeFlashButton;
+    int countflah = 0;
 
     void Start()
     {
-        // Si no se asignó un origen del raycast, usamos el centro de la burbuja (el mismo GameObject)
-        while (_origenRaycast == null)
-        {
+        if (_origenRaycast == null)
             _origenRaycast = transform;
-        }
 
-        while (_rb == null) 
-        {
+        if (_rb == null)
             _rb = this.gameObject.GetComponent<Rigidbody2D>();
-        }
 
         _rb.gravityScale = _gravityScale;
         _rb.freezeRotation = true;
-
-        if (Keyboard.current != null)
-        {
-            _keyboard = Keyboard.current;
-        }
     }
 
     void Update()
     {
-        // Hacer que la burbuja se hunda
         HundirBurbuja();
-
-        // MovePlayer
         PlayerMovement();
-
-        // Rotar el raycast hacia el mouse
         RotarRaycast();
-
-        // Lanzar el raycast para detectar colisiones
         LanzarRaycast();
-
-        //Debug.Log(_rb.velocity.y);
+        
     }
 
-
-    // Función para hacer que la burbuja se hunda
     void HundirBurbuja()
     {
-        if(_rb.velocity.y >= _maxSpeed)
-        {
+        if (_rb.velocity.y >= _maxSpeed)
             _rb.velocity = new Vector2(_rb.velocity.x, _maxSpeed);
-        }
     }
 
     void PlayerMovement()
     {
-        _counter += Time.deltaTime;
-        
-        if (_keyboard.aKey.IsPressed() && _detectInput)
+        if (Input.GetKey(KeyCode.A) && _canHop)
         {
-            //_detectInput = false;
-            _rb.velocity = new Vector2(-_sideMove.x, _sideMove.y);
+            _hopTimeCounter += Time.deltaTime;
+            if (_hopTimeCounter < _hopDuration)
+            {
+                _rb.AddForce(new Vector2(-_sideMove.x, _sideMove.y), ForceMode2D.Impulse);
+            }
+
+            Debug.Log("Hop L: " + _hopTimeCounter);
         }
-        else if (_keyboard.dKey.isPressed && _detectInput)
+        else if (Input.GetKeyUp(KeyCode.A))
         {
-            //_detectInput = false;
-            _rb.velocity = new Vector2(_sideMove.x, _sideMove.y);
+            _canHop = false;
+            _hopTimeCounter = 0.0f;
+        }
+        if (Input.GetKey(KeyCode.D) && _canHop)
+        {
+            _hopTimeCounter += Time.deltaTime;
+            if (_hopTimeCounter < _hopDuration)
+            {
+                _rb.AddForce(new Vector2(_sideMove.x, _sideMove.y), ForceMode2D.Impulse);
+            }
+
+            Debug.Log("Hop R: " + _hopTimeCounter);
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
+        {
+            _canHop = false;
+            _hopTimeCounter = 0.0f;
         }
 
-        if( _keyboard.wKey.isPressed) 
+        if (!_canHop)
         {
-            if(_rb.velocity.y < _minSpeed) 
+            if (_midTimeCounter < _timeBetweenHops)
             {
-                _rb.AddForce(new Vector2(0, _decelerate));
-                //Debug.Log("BREAKING");
+                _midTimeCounter += Time.deltaTime;
+
+                _cooldown.fillAmount = _timeBetweenHops / _midTimeCounter;
             }
             else
             {
-                _rb.velocity = new Vector2(_rb.velocity.x, _minSpeed);
-                Debug.Log("MAX - BREAKING");
+                _midTimeCounter = 0.0f;
+                _canHop = true;
             }
         }
+
+        //else if (Input.GetKey(KeyCode.W))
+        //{
+            
+        //}
     }
 
-    // Función para rotar el raycast en función de la posición del mouse
     void RotarRaycast()
     {
-        // Obtener la posición del mouse en el mundo
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePos.z = transform.position.z; // Asegurarnos de que el raycast solo se mueva en el plano XY (2D)
+        mousePos.z = transform.position.z;
 
-        // Obtener la dirección hacia el mouse
         _raycastDireccion = (mousePos - _origenRaycast.position).normalized;
 
-        // Rotar el origenRaycast para que apunte hacia el mouse
         float angle = Mathf.Atan2(_raycastDireccion.y, _raycastDireccion.x) * Mathf.Rad2Deg;
         _origenRaycast.rotation = Quaternion.Slerp(_origenRaycast.rotation, Quaternion.Euler(0f, 0f, angle), _velocidadRotacion * Time.deltaTime);
     }
 
-    // Función para lanzar el raycast y detectar colisiones
     void LanzarRaycast()
     {
-        // Dibujar el raycast para ver la dirección en la escena (utilizando Gizmos)
         Debug.DrawRay(_origenRaycast.position, _raycastDireccion * _raycastDistancia, Color.red);
 
-        // Lanzar el raycast
-        if (Physics.Raycast(_origenRaycast.position, _raycastDireccion, out _hitInfo, _raycastDistancia, _capaInteractuable))
+        RaycastHit2D hit = Physics2D.Raycast(_origenRaycast.position, _raycastDireccion, _raycastDistancia, _capaInteractuable);
+        if (hit.collider != null)
         {
-            // Si el raycast golpea algo, mostrar la información
-            Debug.Log("Raycast impactó con: " + _hitInfo.collider.name);
+            //Debug.Log("Impactó con: " + hit.collider.name);
+
+            if (hit.collider.CompareTag("Izquierda1"))
+            {
+                Debug.Log("Izquierda");
+                _rb.velocity = new Vector2(velocidadIzquierda1, _rb.velocity.y);
+            }
+            else if (hit.collider.CompareTag("Derecha1"))
+            {
+                Debug.Log("Derecha1");
+                _rb.velocity = new Vector2(-velocidadDerecha1, _rb.velocity.y);
+            }
+
+            if (hit.collider.CompareTag("FlashIzquierda"))
+            {
+                if(Input.GetMouseButtonDown(0) && countflah == 0) 
+                {
+                    Debug.Log("FlashIzq");
+                    FlashIzq.SetActive(true);
+                    countflah++;
+                }
+                else if (Input.GetMouseButtonUp(0))
+                {
+                    FlashIzq.SetActive(false);
+                    ChangeFlash();
+                }
+
+            }
+            else
+            {
+                FlashIzq.SetActive(false);
+            }
+            
+            if (hit.collider.CompareTag("FlashDerecha"))
+            {
+                if(Input.GetMouseButtonDown(0) && countflah == 0)
+                {
+                Debug.Log("FlashDer");
+                FlashDer.SetActive(true);
+                countflah++;
+                }
+                else if (Input.GetMouseButtonUp(0))
+                {
+                    FlashDer.SetActive(false);
+                    ChangeFlash();
+                }
+
+            }
+            else
+            {
+                FlashDer.SetActive(false);
+            }
+
+            if (hit.collider.CompareTag("Freno"))
+            {
+
+                if (Input.GetMouseButton(0))
+                {
+                    Debug.Log("Freno");
+                    if (_rb.velocity.y < _minSpeed)
+                    {
+                        if (_rb.velocity.x > 0)
+                            _rb.AddForce(new Vector2(-1, _decelerate));
+
+                        else if (_rb.velocity.x < 0)
+                            _rb.AddForce(new Vector2(1, _decelerate));
+
+                        Debug.Log("BREAKING");
+                    }
+                    else
+                    {
+                        if (_rb.velocity.x > 0)
+                            _rb.velocity = new Vector2(_rb.velocity.x - 1, _minSpeed);
+
+                        else if (_rb.velocity.x < 0)
+                            _rb.velocity = new Vector2(_rb.velocity.x + 1, _minSpeed);
+
+                        Debug.Log("MAX - BREAKING");
+                    }
+                }
+                else if (Input.GetMouseButtonUp(0))
+                {
+                    Debug.Log("NoFreno");
+                }
+                
+          
+                
+            }
+
+        }
+        else
+        {
+            Debug.Log("NoColisiona");
         }
     }
 
-    // Dibujar el raycast con Gizmos en la vista de la escena
+    void ChangeFlash()
+    {
+        if(countflah == 1)
+        {
+            _changeFlashButton.SetActive(true);
+        }
+    }
+
     void OnDrawGizmos()
     {
         if (_origenRaycast == null) return;
 
-        // Dibujar el raycast en Gizmos
+
         Gizmos.color = Color.red;
         Gizmos.DrawRay(_origenRaycast.position, _raycastDireccion * _raycastDistancia);
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        
-    }
-
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        if (collision == null) return;
-        else
-        {
-            Debug.Log(_rb.velocity);
-        }
     }
 }
