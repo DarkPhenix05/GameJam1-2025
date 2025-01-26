3c0c532fbb9f24891eeb2ca063b056e7f2e5423c
FranciscoPS <francisco.ps1996@gmail.com>
2025-01-25 18:21:39 -0600

    feat: relative initial position and timers assigned

--- Assets/Subble-Bubble/Scripts/Enemies/swordfish/movement_swordfish.cs
+++ Assets/Subble-Bubble/Scripts/Enemies/swordfish/movement_swordfish.cs
@@ -4,25 +4,59 @@ using UnityEngine;
 
 public class movement_swordfish : MonoBehaviour
 {
-    [SerializeField] float speed = 5f;
+    [SerializeField] private float speed = 7f;
+    [SerializeField] private Transform player;
+    [SerializeField] private Vector2 relativeOffset = new Vector2(0, -4.5f);
+
+    private float holdTime = 3f;
+    private float destructionTime = 5f;
+    private bool isMovingStraight = false;
 
     void Start()
     {
+        if (player)
+        {
+            Vector3 newPosition = player.position + new Vector3(relativeOffset.x, relativeOffset.y, 0);
+            transform.position = newPosition;
+        }
 
+        StartCoroutine(HandleMovement());
     }
 
     void Update()
     {
-        Mover();
+        if (isMovingStraight)
+        {
+            Move();
+        }
+        else if (player)
+        {
+            Vector3 newPosition = player.position + new Vector3(relativeOffset.x, relativeOffset.y, 0);
+            transform.position = newPosition;
+        }
     }
 
-    private void Mover()
+    private void Move()
     {
         transform.Translate(Vector2.up * speed * Time.deltaTime);
     }
 
+    private IEnumerator HandleMovement()
+    {
+        yield return new WaitForSeconds(holdTime);
+
+        isMovingStraight = true;
+
+        yield return new WaitForSeconds(destructionTime);
+
+        gameObject.SetActive(false);
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(gameObject);
+        if (collision.gameObject.CompareTag("RaycastOrigen"))
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
